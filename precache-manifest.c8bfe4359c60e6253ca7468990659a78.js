873921b309d59cd4c8360569c9802f46a3424dc5
kevin <kevin@betacode.io>
2020-06-16 13:38:00 +0800

    Updates

---
+++ precache-manifest.c8bfe4359c60e6253ca7468990659a78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab92caedeea35f0757bae29590088b5",
+    "url": "/react-storybook-tutorial/index.html"
+  },
+  {
+    "revision": "11206c4539c12dfd7441",
+    "url": "/react-storybook-tutorial/static/css/main.45b2bdba.chunk.css"
+  },
+  {
+    "revision": "1744d3fc56552f5da228",
+    "url": "/react-storybook-tutorial/static/js/2.1da7cccb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-storybook-tutorial/static/js/2.1da7cccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11206c4539c12dfd7441",
+    "url": "/react-storybook-tutorial/static/js/main.d8d73ea6.chunk.js"
+  },
+  {
+    "revision": "904c245beb8ca4990833",
+    "url": "/react-storybook-tutorial/static/js/runtime-main.9bda0247.js"
+  }
+]);
